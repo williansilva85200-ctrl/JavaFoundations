faf7e93b2d44ff1dcac7b77e1d14e1f534811918
williansilva85200-ctrl <williansilva85200@gmail.com>
2025-11-25 16:59:09 -0300

    Estrutura condicional

---
+++ 02 - Estrutura de decisao/Estrutura de decisao/src/exercicios/Decisao.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class Decisao {
+    public static void main(String[] args) {
+
+        int nota = 50;
+
+        if (nota >= 70){
+            System.out.println("Aprovado");
+
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
